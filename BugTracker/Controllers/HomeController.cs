be75c19300a0a56105931d5c8f1ab131e541eda7
Werichinilson <Werichinilson.Ataliba@coxautoinc.com>
2019-01-23 15:53:41 -0200

    firat commit

---
+++ BugTracker/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BugTracker.Models;
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using BugTracker.Data;
+
+namespace BugTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public MongoDBRepository mongoDB;
+        public HomeController(IOptions<Settings> settings)
+        {
+           mongoDB = new MongoDBRepository(settings);
+        }
+
+        public IActionResult Index()
+        {
+            return Json(mongoDB.DataBase.Client.Cluster.Description);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BugTracker/Data/MongoDBRepository.cs
@@ -0,0 +1,28 @@
+﻿using BugTracker.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data
+{
+    public class MongoDBRepository
+    {
+        public readonly IMongoDatabase DataBase;
+
+        public MongoDBRepository(IOptions<Settings> settings)
+        {
+            try
+            {
+                var mclient = new MongoClient(settings.Value.ConnectionString);
+                DataBase = mclient.GetDatabase(settings.Value.Database);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("There was a problem connecting to the MongoDB database", ex);
+            }
+        }
+    }
+}
